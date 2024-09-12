442eb8fc83955f6517518c999dff270c7b1b4a4b
Akseenja <klasik1995@bk.ru>
2024-09-12 12:29:41 +0300

    init: start cssMemeSlider-task (Thu, Sep 12, 2024 12:29:41 PM)

---
+++ scripts/list.js
@@ -0,0 +1,26 @@
+const list = [
+  {
+    text: "random image one",
+    path: "./assets/first.jpg",
+  },
+  {
+    text: "random image two",
+    path: "./assets/second.jpg",
+  },
+  {
+    text: "random image 3",
+    path: "./assets/third.jpg",
+  },
+  {
+    text: "random image четыре",
+    path: "./assets/fourth.jpg",
+  },
+  {
+    text: "random image 2 наоборот (5)",
+    path: "./assets/fifth.jpg",
+  },
+  {
+    text: "random image 9 (наоборот)",
+    path: "./assets/sixth.jpg",
+  },
+]
